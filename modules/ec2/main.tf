9a854530879cca8bfd3b9d5fc3ef89279e0e13e5
hams77 <hamsiniabiel@gmail.com>
2024-01-16 17:59:06 +0530

    first commit

--- modules/ec2/main.tf
+++ modules/ec2/main.tf
@@ -2,10 +2,10 @@ resource "aws_instance" "main" {
     instance_type = var.instance_type
     ami = var.ami
     subnet_id = var.subnet_id
-    associate_public_ip_address = true
+    
     vpc_security_group_ids = [var.sg_id]
     key_name = var.key_name
-associate_public_ip_address = true
+
 monitoring           =   var.monitoring
   ebs_optimized = var.ebs_optimized
   associate_public_ip_address = var.associate_public_ip_address
